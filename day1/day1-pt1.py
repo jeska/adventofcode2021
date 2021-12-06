8aba6a4a4c2768d2c25179055270b33382643154
Jeska <jessicamdavid@gmail.com>
2021-12-05 21:03:22 -0500

    More cleaning up for Day 1

--- day1/day1-pt1.py
+++ day1/day1-pt1.py
@@ -1,22 +1,15 @@
 def main(inf: str):
 
-    increases = 0
     with open(inf, "r") as input_file:
-        first_depth, remaining_depths = (
-            int(input_file.readline()),
-            input_file.readlines(),
-        )
+        depths = [int(line) for line in input_file.readlines()]
 
-    for line in remaining_depths:
-        current_depth = int(line)
-        increases += 1 if current_depth > first_depth else 0
-        first_depth = current_depth
-
-    return increases
+    return sum([1 for i in range(0, len(depths) - 1) if depths[i] < depths[i + 1]])
 
 
 if __name__ == "__main__":
     sample_result = main("day1-sample.txt")
+    print(sample_result)
     assert sample_result == 7
     full_result = main("day1-full.txt")
+    print(full_result)
     assert full_result == 1139
