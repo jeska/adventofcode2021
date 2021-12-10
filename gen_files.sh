286f891ee805bea4fcb7ba45f5ba4a76b724d9b7
Jeska <jessicamdavid@gmail.com>
2021-12-10 10:22:27 -0500

    template and generation updates

--- gen_files.sh
+++ gen_files.sh
@@ -9,3 +9,5 @@ mv dayN-sample.txt "${DAY_STR}"-sample.txt
 mv dayN-full.txt "${DAY_STR}"-full.txt
 mv dayN-pt1.py "${DAY_STR}"-pt1.py
 mv dayN-pt2.py "${DAY_STR}"-pt2.py
+
+sed -i '' "s/dayN/${DAY_STR}/g" "${DAY_STR}"-pt*.py
--- template/dayN/dayN-pt1.py
+++ template/dayN/dayN-pt1.py
@@ -8,7 +8,7 @@ def main(inf: str):
 if __name__ == "__main__":
     sample_result = main("dayN-sample.txt")
     print(sample_result)
-    # assert sample_result == 0
-    full_result = main("dayN-full.txt")
-    print(full_result)
+    assert sample_result == 0
+    # full_result = main("dayN-full.txt")
+    # print(full_result)
     # assert full_result == 0
